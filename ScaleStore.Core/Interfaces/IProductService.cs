044389dea34b02fa78cef31f1678b6ad589b6e02
the-sauravkumar <thesauravkumar@hotmail.com>
2026-08-07 23:05:35 +0530

    Add IProductService interface with async product methods
    Introduced IProductService interface defining async CRUD operations for products using DTOs. All methods return Task-based results to support asynchronous workflows.

---
+++ ScaleStore.Core/Interfaces/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using ScaleStore.Core.DTOs;
+using ScaleStore.Core.DTOs.Product;
+using System.Threading.Tasks;
+
+namespace ScaleStore.Core.Interfaces
+{
+    public interface IProductService
+    {
+        // Promises to return a list of product
+        Task<IEnumerable<ProductResponseDto>> GetAllProductsAsync();
+        
+        // Promises to return a single product (nullable)
+        Task<ProductResponseDto?> GetProductById(int id);
+        Task <ProductCreatedResponseDto> CreateProductAsync(CreateProductDto dto);
+
+        // Promises to return true if update succeeded, false if not found
+        Task<bool> UpdateProductAsync(int id, UpdateProductDto dto);
+
+        // true if delete, false if not found
+        Task<bool> DeleteProductAsync(int id);
+    }
+}
